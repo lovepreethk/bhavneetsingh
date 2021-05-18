8ed271b7a802d794c0841b4700689c9998f2668a
lovepreethk <84321193+lovepreethk@users.noreply.github.com>
2021-05-18 08:12:12 +0530

    Update MyScript.js

--- MyScript.js
+++ MyScript.js
@@ -1,4 +1,4 @@
 function aboutMe{    
-    var int = document.getIdByElement().value;
+    var int = document.getIdByElement(jatt).value;
     alert('Comming shortly with this term, I am learning this'); 
 }
